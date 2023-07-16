1563af8d2eba612ab40cac17b8273fec7c899600
Lymeng <lymengouk2610@gmail.com>
2023-07-16 22:36:08 +0700

    firstcommit

---
+++ src/main/java/com/springboot/app/model/Employee.java
@@ -0,0 +1,11 @@
+package com.springboot.app.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    public long Id;
+    public String firstName;
+    public String lastName;
+    public String email;
+}
